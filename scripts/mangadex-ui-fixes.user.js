8baa385eab04ff50a04d8f715f135e0df0dd7379
Amr Abulseoud <amrabulseoud@gmail.com>
2020-04-17 15:51:07 +0200

    [FEAT]: created mangadex-ui-fixes.user.js

---
+++ scripts/mangadex-ui-fixes.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        mangadex ui fixes
+// @namespace   Violentmonkey Scripts
+// @match       https://mangadex.cc/*
+// @match       https://mangadex.org/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 1/5/2020, 5:56:12 PM
+// ==/UserScript==
+
+document.getElementById("content").style["max-width"] = "95%"
