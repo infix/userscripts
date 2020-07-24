22cb102fe069310cb9f03e7367f1692f2e3e347e
Amr Abulseoud <amrabulseoud@gmail.com>
2020-07-24 18:47:45 +0200

    [REFACTOR]: cleaned up implementation by using rxjs and added retrying on network failure, and completely overhauled the thumbnail logic

--- scripts/mangadex-follows.user.js
+++ scripts/mangadex-follows.user.js
@@ -3,189 +3,96 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://mangadex.org/follows
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      -
 // ==/UserScript==
 
-/** @param {string} html */
-const parseHtml = (html) => new DOMParser().parseFromString(html, "text/html");
+const injectScript = (src) =>
+  new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.addEventListener("load", resolve);
+    script.addEventListener("error", (e) => reject(e.error));
+    document.head.appendChild(script);
+  });
 
-/**
- * @param {Date} d1
- * @param {Date} d2
- * @returns {number}
- */
-const dateDiffInMinutes = (d1, d2) => Math.floor((d1 - d2) / 1000 / 60);
+(async () => {
+  await injectScript("https://unpkg.com/rxjs@6.6.0/bundles/rxjs.umd.min.js");
 
-let lastUpdate = new Date();
+  const { merge, interval, fromEvent, Subject } = rxjs;
+  const { filter, scan, mergeMap } = rxjs.operators;
+  const { map, tap, delay, retryWhen, take, switchMap } = rxjs.operators;
 
-const batotoBtn = document.querySelector("#content > ul > li:nth-child(4)");
-const lastUpdated = document.createElement("li");
-lastUpdated.style.alignSelf = "center";
+  const parseHtml = (html) =>
+    new DOMParser().parseFromString(html, "text/html");
 
-const setLastUpdated = () => {
-  lastUpdated.innerText = `Last updated: ${dateDiffInMinutes(
-    new Date(),
-    lastUpdate,
-  )} mins ago`;
-};
+  const dateDiffInMinutes = (d1, d2) => Math.floor((d1 - d2) / 1000 / 60);
 
-setLastUpdated();
+  const batotoBtn = document.querySelector("#content > ul > li:nth-child(4)");
+  const lastUpdated = document.createElement("li");
+  lastUpdated.style.alignSelf = "center";
+  batotoBtn.after(lastUpdated);
 
-batotoBtn.after(lastUpdated);
-let isUpdating = false;
+  const isUpdatingBS = new rxjs.BehaviorSubject(false);
+  const imagesSub = new Subject();
+  // noinspection ES6MissingAwait
+  imagesSub
+    .pipe(
+      scan((prevLastUpdate, lastUpdate) => {
+        const diff = dateDiffInMinutes(new Date(), prevLastUpdate);
+        lastUpdated.innerText = `Last updated: ${diff} mins ago`;
+        return lastUpdate;
+      }, new Date()),
+    )
+    .subscribe(() => {
+      const getMangaId = (e) =>
+        e.firstElementChild
+          .querySelector("a")
+          .href.replace("https://mangadex.org/title/", "")
+          .split("/")[0];
 
-const imageCache = new Map();
+      const queryStr = ".chapter-container > .row.no-gutters:not(:first-child)";
+      const chapters = [...document.querySelectorAll(queryStr)];
 
-const initialize = () => {
-  const cache = JSON.parse(localStorage.getItem("image-cache") || "{}");
-  Object.entries(cache).forEach(([k, v]) => imageCache.set(k, v));
-};
-
-initialize();
-
-const writeToLocalStorage = () => {
-  const imageCacheObject = Object.fromEntries([...imageCache.entries()]);
-  localStorage.setItem("image-cache", JSON.stringify(imageCacheObject));
-};
-
-/**
- * @param {string} id
- * @return {{cached: boolean, image: string}}
- */
-const getImage = async (id) => {
-  if (imageCache.has(id)) return { cached: true, image: imageCache.get(id) };
-
-  const IMAGE_BASE_URL = `https://mangadex.org/images/manga/`;
-  const urls = [
-    { url: `${IMAGE_BASE_URL}${id}.jpg`, type: "image/jpeg" },
-    { url: `${IMAGE_BASE_URL}${id}.png`, type: "image/png" },
-    { url: `${IMAGE_BASE_URL}${id}.jpeg`, type: "image/jpeg" },
-  ];
-
-  for (const { url, type } of urls)
-    try {
-      const base64 = await downscaleImage(url, 60, type);
-      imageCache.set(id, base64);
-      return { cached: false, image: base64 };
-    } catch (e) {
-      console.log(`Failed to fetch ${url}.`);
-    }
-};
-
-/** @param {{mangaId: string, element: HTMLElement}} chapter */
-async function setImage(chapter) {
-  const img = document.createElement("img");
-  const { cached, image } = await getImage(chapter.mangaId);
-  img.src = image;
-  chapter.element.firstElementChild.firstElementChild.before(img);
-  chapter.element.children[2].firstElementChild.style.height = "100%";
-  img.height = "80";
-  img.style.paddingRight = "1rem";
-  return cached;
-}
-
-async function downscaleImage(dataUrl, newWidth, imageType = "image/jpeg") {
-  /** @param {HTMLImageElement} img */
-  const imageLoad = (img) =>
-    new Promise((resolve, reject) => {
-      img.onload = () => resolve();
-      img.onerror = () => reject();
+      chapters
+        .filter((e) => e.firstElementChild.innerText !== "")
+        .map((e) => ({ element: e, mangaId: getMangaId(e) }))
+        .forEach((chapter) => {
+          const img = document.createElement("img");
+          img.src = `https://mangadex.org/images/manga/${chapter.mangaId}.thumb.jpg`;
+          chapter.element.firstElementChild.firstElementChild.before(img);
+          chapter.element.children[2].firstElementChild.style.height = "100%";
+          img.height = "80";
+          img.style.paddingRight = "1rem";
+        });
     });
-
-  // Create a temporary image so that we can compute the height of the downscaled image.
-  const image = new Image();
-  image.src = dataUrl;
-
-  await imageLoad(image);
-
-  const oldWidth = image.width;
-  const oldHeight = image.height;
-  const newHeight = Math.floor((oldHeight / oldWidth) * newWidth);
-
-  // Create a temporary canvas to draw the downscaled image on.
-  const canvas = document.createElement("canvas");
-  canvas.width = newWidth;
-  canvas.height = newHeight;
-
-  // Draw the downscaled image on the canvas and return the new data URL.
-  const ctx = canvas.getContext("2d");
-  ctx.drawImage(image, 0, 0, newWidth, newHeight);
-  return canvas.toDataURL(imageType);
-}
-
-/** @param {number} ms */
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-const setAllImages = async () => {
-  const MANGADEX_TITLE_URL = "https://mangadex.org/title/";
-
-  const getMangaId = (e) =>
-    e.firstElementChild
-      .querySelector("a")
-      .href.replace(MANGADEX_TITLE_URL, "")
-      .split("/")[0];
-
-  /** @type {{mangaId: string, element: HTMLElement}[]} */
-  const chapterElements = [
-    ...document.querySelectorAll(
-      ".chapter-container > .row.no-gutters:not(:first-child)",
+  imagesSub.next(new Date());
+  // noinspection ES6MissingAwait
+  merge(
+    interval(5 * 60 * 1000),
+    fromEvent(document, "keydown").pipe(
+      filter((ev) => ev.key === "r" && !ev.ctrlKey),
+      tap((ev) => ev.preventDefault()),
     ),
-  ]
-    .filter((e) => e.firstElementChild.innerText !== "")
-    .map((e) => ({ element: e, mangaId: getMangaId(e) }));
-
-  for (const chapter of chapterElements) {
-    try {
-      const isCached = await setImage(chapter);
-      if (!isCached) {
-        writeToLocalStorage();
-        await sleep(1000);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
-};
-
-setAllImages();
-
-const updateFollows = async () => {
-  if (isUpdating) return;
-
-  isUpdating = true;
-  lastUpdated.innerText = "Updating...";
-
-  try {
-    console.log("Updating");
-    const resp = await fetch("https://mangadex.org/follows");
-    const html = await resp.text();
-
-    const dom = parseHtml(html);
-
-    const newChapters = dom.querySelector("#chapters");
-
-    if (newChapters)
-      document.querySelector("#chapters").replaceWith(newChapters);
-  } finally {
-    lastUpdate = new Date();
-    isUpdating = false;
-    setLastUpdated();
-    await setAllImages();
-  }
-};
-
-setInterval(() => {
-  if (dateDiffInMinutes(new Date(), lastUpdate) > 4) {
-    // update every 5 minutes
-    updateFollows();
-  }
-
-  setLastUpdated();
-}, 1000 * 30); // check every 30 seconds
-
-document.addEventListener("keydown", ({ key }) => {
-  if (key === "r" && !isUpdating) {
-    updateFollows();
-  }
-});
+  )
+    .pipe(
+      filter(() => !isUpdatingBS.getValue()),
+      tap(() => {
+        console.log("Updating");
+        isUpdatingBS.next(true);
+        lastUpdated.innerText = "Updating...";
+      }),
+      mergeMap(() =>
+        rxjs.fetch.fromFetch("https://mangadex.org/follows").pipe(
+          switchMap((response) => rxjs.from(response.text())),
+          map((text) => parseHtml(text)),
+          map((dom) => dom.querySelector("#chapters")),
+          retryWhen((errors) => errors.pipe(delay(1000), take(2))),
+        ),
+      ),
+      tap((newChapters) =>
+        document.querySelector("#chapters").replaceWith(newChapters),
+      ),
+    )
+    .subscribe(() => imagesSub.next(new Date()));
+})();
