70ddde1afe72bfe83c9e9ab1ca002480271f8fe5
Amr Abulseoud <amrabulseoud@gmail.com>
2020-07-24 19:44:49 +0200

    [FIX]: fixed last updated not updating correctly

--- scripts/mangadex-follows.user.js
+++ scripts/mangadex-follows.user.js
@@ -20,79 +20,76 @@ const injectScript = (src) =>
   await injectScript("https://unpkg.com/rxjs@6.6.0/bundles/rxjs.umd.min.js");
 
   const { merge, interval, fromEvent, Subject } = rxjs;
-  const { filter, scan, mergeMap } = rxjs.operators;
+  const { BehaviorSubject, of } = rxjs;
+  const { fromFetch } = rxjs.fetch;
+  const { filter, mergeMap } = rxjs.operators;
   const { map, tap, delay, retryWhen, take, switchMap } = rxjs.operators;
 
   const parseHtml = (html) =>
     new DOMParser().parseFromString(html, "text/html");
-
-  const dateDiffInMinutes = (d1, d2) => Math.floor((d1 - d2) / 1000 / 60);
 
   const batotoBtn = document.querySelector("#content > ul > li:nth-child(4)");
   const lastUpdated = document.createElement("li");
   lastUpdated.style.alignSelf = "center";
   batotoBtn.after(lastUpdated);
 
-  const isUpdatingBS = new rxjs.BehaviorSubject(false);
+  const lastUpdateBS = new BehaviorSubject(new Date());
+  merge(interval(30 * 1000), lastUpdateBS).subscribe(() => {
+    const dateDiffInMinutes = (d1, d2) => Math.floor((d1 - d2) / 1000 / 60);
+    const diff = dateDiffInMinutes(new Date(), lastUpdateBS.getValue());
+    lastUpdated.innerText = `Last updated: ${diff} mins ago`;
+  });
+
+  const isUpdatingBS = new BehaviorSubject(false);
+  isUpdatingBS.subscribe((value) => {
+    if (value) {
+      lastUpdated.innerText = "Updating...";
+    } else {
+      lastUpdateBS.next(new Date());
+    }
+  });
+
   const imagesSub = new Subject();
   // noinspection ES6MissingAwait
-  imagesSub
-    .pipe(
-      scan((prevLastUpdate, lastUpdate) => {
-        const diff = dateDiffInMinutes(new Date(), prevLastUpdate);
-        lastUpdated.innerText = `Last updated: ${diff} mins ago`;
-        return lastUpdate;
-      }, new Date()),
-    )
-    .subscribe(() => {
-      const getMangaId = (e) =>
-        e.firstElementChild
+  merge(of(1), imagesSub).subscribe(() => {
+    const queryStr = ".chapter-container > .row.no-gutters:not(:first-child)";
+    [...document.querySelectorAll(queryStr)]
+      .filter((e) => e.firstElementChild.innerText !== "")
+      .forEach((e) => {
+        const mangaId = e.firstElementChild
           .querySelector("a")
           .href.replace("https://mangadex.org/title/", "")
           .split("/")[0];
 
-      const queryStr = ".chapter-container > .row.no-gutters:not(:first-child)";
-      const chapters = [...document.querySelectorAll(queryStr)];
+        const img = document.createElement("img");
+        img.src = `https://mangadex.org/images/manga/${mangaId}.thumb.jpg`;
+        e.firstElementChild.firstElementChild.before(img);
+        e.children[2].firstElementChild.style.height = "100%";
+        img.height = "80";
+        img.style.paddingRight = "1rem";
+      });
+  });
 
-      chapters
-        .filter((e) => e.firstElementChild.innerText !== "")
-        .map((e) => ({ element: e, mangaId: getMangaId(e) }))
-        .forEach((chapter) => {
-          const img = document.createElement("img");
-          img.src = `https://mangadex.org/images/manga/${chapter.mangaId}.thumb.jpg`;
-          chapter.element.firstElementChild.firstElementChild.before(img);
-          chapter.element.children[2].firstElementChild.style.height = "100%";
-          img.height = "80";
-          img.style.paddingRight = "1rem";
-        });
-    });
-  imagesSub.next(new Date());
+  const refreshKeyDown = fromEvent(document, "keydown").pipe(
+    filter((ev) => ev.key === "r" && !ev.ctrlKey),
+    tap((ev) => ev.preventDefault()),
+  );
   // noinspection ES6MissingAwait
-  merge(
-    interval(5 * 60 * 1000),
-    fromEvent(document, "keydown").pipe(
-      filter((ev) => ev.key === "r" && !ev.ctrlKey),
-      tap((ev) => ev.preventDefault()),
-    ),
-  )
+  merge(interval(5 * 60 * 1000), refreshKeyDown)
     .pipe(
       filter(() => !isUpdatingBS.getValue()),
-      tap(() => {
-        console.log("Updating");
-        isUpdatingBS.next(true);
-        lastUpdated.innerText = "Updating...";
-      }),
+      tap(() => isUpdatingBS.next(true)),
       mergeMap(() =>
-        rxjs.fetch.fromFetch("https://mangadex.org/follows").pipe(
+        fromFetch("https://mangadex.org/follows").pipe(
           switchMap((response) => rxjs.from(response.text())),
-          map((text) => parseHtml(text)),
-          map((dom) => dom.querySelector("#chapters")),
+          map((text) => parseHtml(text).querySelector("#chapters")),
           retryWhen((errors) => errors.pipe(delay(1000), take(2))),
         ),
       ),
-      tap((newChapters) =>
-        document.querySelector("#chapters").replaceWith(newChapters),
-      ),
     )
-    .subscribe(() => imagesSub.next(new Date()));
+    .subscribe((newChapters) => {
+      document.querySelector("#chapters").replaceWith(newChapters);
+      isUpdatingBS.next(false);
+      imagesSub.next();
+    });
 })();
